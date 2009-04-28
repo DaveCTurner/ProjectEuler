1c2614e5bd01178f609de1a8ffe1ad48bb5494f9
Dave Turner <dave.c.turner@gmail.com>
2009-04-28 23:00:10 +0100

    Reordered search in problem44 solution to make it faster

--- problem44.pl
+++ problem44.pl
@@ -2,7 +2,7 @@
 
 use strict;
 
-my $n = 100000;
+my $n = 5000;
 
 my @pents = (0);
 my %pentsHash = ();
@@ -14,12 +14,12 @@ for (my $i = 1; $i < $n; $i++) {
 }
 
 for (my $i = 1; $i < $n; $i++) {
-	for (my $j = $i + 1; $j < $n; $j++) {
+	for (my $j = 1; $j < $i; $j++) {
 		if ($pentsHash{$pents[$i] + $pents[$j]}
-			&& ($pentsHash{$pents[$j] - $pents[$i]})) {
+			&& ($pentsHash{$pents[$i] - $pents[$j]})) {
 
 			printf "%d %d %d %d %d\n",
-				$pents[$j] - $pents[$i], $i, $j, $pents[$i], $pents[$j];
+				$pents[$i] - $pents[$j], $i, $j, $pents[$i], $pents[$j];
 		}
 	}
 }
