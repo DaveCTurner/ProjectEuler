aebacc4cf5a0c567b86676724940f4ae27ef2df6
Dave Turner <dave.c.turner@gmail.com>
2009-05-01 20:40:18 +0100

    Solved problem 59 (in >1 minute tho)

--- problem59.pl
+++ problem59.pl
@@ -5,35 +5,42 @@ use strict;
 chomp $ARGV[0];
 my @encoded = split /,/, $ARGV[0];
 
-my @passcode = map { ord } ('g', 'o', 'd');
-
-#for ($passcode[0] = 0; $passcode[0] <= 26; $passcode[0] += 1) {
-#for ($passcode[1] = 0; $passcode[1] <= 26; $passcode[1] += 1) {
-#for ($passcode[2] = 0; $passcode[2] <= 26; $passcode[2] += 1) {
-
-print join ' ', @passcode;
-print "\n";
-
-my $total = 0;
-
-for (my $i = 0; $i < $#encoded; $i++) {
-	my $passcodePosition = $i % 3;
+sub decode {
+	my ($passcode) = @_;
+	my @passcode = unpack 'C3', $passcode;
 	
-	my $decoded = $encoded[$i] ^ $passcode[$passcodePosition];
-	$total += $decoded;
-#	$decoded = $decoded & 31;
-#	$decoded = $decoded | 96;
-	print chr($decoded);
-	print " ";
-	print $decoded;
-	print "\n";
+	my @decoded = ();
+
+	for (my $i = 0; $i <= $#encoded; $i++) {
+		my $passcodePosition = $i % 3;
+		push @decoded, $encoded[$i] ^ $passcode[$passcodePosition];
+	}
+
+	return pack 'C*', @decoded;
 }
-print "\n";
 
-printf "%d\n", $total;
+my @passcodeTry = (0, 0, 0);
 
-#}
-#}
-#}
+for ($passcodeTry[0] = 1; $passcodeTry[0] <= 26; $passcodeTry[0] += 1) {
+	for ($passcodeTry[1] = 1; $passcodeTry[1] <= 26; $passcodeTry[1] += 1) {
+		for ($passcodeTry[2] = 1; $passcodeTry[2] <= 26; $passcodeTry[2] += 1) {
 
+			my $password = pack 'C3', (map { $_ + 96 } @passcodeTry);
 
+			my $decoded = decode($password);
+
+			my $decoded_UC = uc $decoded;
+
+			if ($decoded_UC =~ / THE/ &&
+				$decoded_UC =~ / AND /) {
+
+				print "$password\n";
+				my $total = 0;
+				map { $total += $_ } unpack 'C*', $decoded;
+				print "$total\n";
+				print "$decoded\n\n";
+			}
+
+		}
+	}
+}
