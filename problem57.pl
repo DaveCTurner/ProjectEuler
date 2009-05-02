beef3c027ecad3306341046a28c497e4311d5964
Dave Turner <dave.c.turner@gmail.com>
2009-05-02 10:10:54 +0100

    Solved problem 57 in Perl

--- problem57.pl
+++ problem57.pl
@@ -6,9 +6,16 @@ use strict;
 my $n = Math::BigInt->new(3);
 my $d = Math::BigInt->new(2);
 
+my $longerNumeratorCount = 0;
+
 for (my $i = 1; $i <= 1000; $i++) {
-	printf("%d: %d/%d\n", $i, $n, $d);
+#	printf("%d: %s/%s\n", $i, $n, $d);
+	if ($n->length() > $d->length()) {
+		$longerNumeratorCount += 1;
+	}
 	$n->badd($d);
 	($n, $d) = ($d, $n);
 	$n->badd($d);
 }
+
+printf("%d\n", $longerNumeratorCount);
