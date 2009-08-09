5323872e1f01f52f3f6ad31896b69418c60ba672
Dave Turner <dave.c.turner@gmail.com>
2009-08-09 19:23:40 +0100

    Added problem 100 solution - brute force, might be too slow

---
+++ problem100.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use Math::BigInt;
+
+my $n = Math::BigInt->new(0);
+my $n_acc = Math::BigInt->new(1); # = 2n(n-1)+1
+
+my $m = Math::BigInt->new(0); # Square this
+my $m_acc = Math::BigInt->new(0); # = m^2
+
+my $threshold = Math::BigInt->new('1000000000000');
+
+while(1) {
+	my $cmp = $n_acc->bcmp($m_acc);
+
+	if (0 == $cmp) {
+		my $p = $n->copy()->bsub(1)->bmul(2)->bmul($n)->badd(1)->bsqrt()
+			->badd(1)->bdiv(2)->bstr();
+		printf("n = %s, m = %s, 2n(n-1)+1 = m^2 = %s, p = %s\n", $n->bstr(),
+			$m->bstr(), $n_acc->bstr(), $p);
+		if ($threshold->bcmp($n) < 0) {
+			printf("%s\n", $p);
+			last;
+		}
+	}
+
+	if (0 <= $cmp) {
+		$m_acc->badd($m->copy()->bmul(2)->badd(1));
+		$m->binc();
+	} else {
+		$n_acc->badd($n->copy()->bmul(4));
+		$n->binc();
+	}
+}
